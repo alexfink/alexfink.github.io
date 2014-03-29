5a754952c6939936d152ecd1166b79703c39013a
Alex Fink <000024@gmail.com>
2014-03-29 20:58:57 +0000

    more primes!

--- dive/js/html_actuator.js
+++ dive/js/html_actuator.js
@@ -8,7 +8,7 @@ function HTMLActuator() {
 
   this.score = 0;
   
-  this.overlayPrimes = [7, 11, 13, 17, 19, 23, 29, 31];
+  this.overlayPrimes = [7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71];
 }
 
 HTMLActuator.prototype.actuate = function (grid, metadata) {
