a9ef693bd9f3beda81fa131f489c82ce47c148d7
Vynce Montgomery <vynce@codehs.com>
2014-03-29 12:38:00 -0700

    instantiation order fix?

--- dive/js/local_score_manager.js
+++ dive/js/local_score_manager.js
@@ -21,7 +21,7 @@ window.fakeStorage = {
 function LocalScoreManager() {
   var localSupported = !!window.localStorage;
 
-  this.key     = "bestScoreDiv_mode_" + game.gameMode;
+  this.key     = "bestScoreDiv_mode_" + (game ? game.gameMode : '');
   this.storage = localSupported ? window.localStorage : window.fakeStorage;
 }
 
